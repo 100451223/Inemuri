e8d64a59c68151adff7e02e879875adf2de67c6b
Daniel <100451223@alumnos.uc3m.es>
2023-02-20 11:37:04 +0100

    Search bar now allows to chnage destination without resetting

--- inputBarSearch.js
+++ inputBarSearch.js
@@ -1,36 +1,49 @@
 // On searching on the input bar, add a marker and change the map's active view
 // While the map view changes, the 'destination' only changes if there are no active markers!
 
-document.getElementById("destInput").addEventListener("keyup", (event) => {
-  if (event.key === "Enter") {
-    destUserInput = event.target.value;
-    event.target.value = "";
-    setCourse();
-  }
-})
 
+
+/* Set the course when entering an address on the input bar and clicking on the search button */
 document.getElementById("searchButton").addEventListener("click", () => {
   searchBar = document.getElementById("destInput");
   destUserInput = searchBar.value;
   searchBar.value = "";
-  setCourse();
+  if(currentDestination != null) {
+    mymap.removeLayer(currentDestination.mrk);
+  }
+  setCourse(destUserInput, mymap, started).then(resp => currentDestination = resp);
 })
 
-function setCourse() {
+/* Set the course when entering an address on the input bar and clicking intro */
+document.getElementById("destInput").addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    destUserInput = event.target.value;
+    event.target.value = "";
+    if(currentDestination != null) {
+      mymap.removeLayer(currentDestination.mrk);
+    }
+    setCourse(destUserInput, mymap, started).then(resp => currentDestination = resp);
+  }
+})
+
+function setCourse(address, map, started) {
   // Add a marker to the location that the user inputted
 
-  addToLatlng(destUserInput).then(resp => {
+  return addToLatlng(address).then(resp => {
     if (resp != undefined) {
 
-      if (mymap != undefined) {
-        mymap.setView([resp.lat, resp.lng], 20);
+      if (map != undefined) {
+        map.setView([resp.lat, resp.lng], 20);
 
-        if (!started && currentDestination == null) {
-          currentDestination = resp;
-          currentDestination.mrk = addMarker(started, mymap, resp);
+        if (!started) {
+          let currentDest = resp;
+          currentDest.mrk = addMarker(started, map, resp);
 
           const subtitle = document.getElementById("destination");
           subtitle.innerText = "Destino: " + resp.add;
+
+          return currentDest;
+
         }
       }
     } else {
